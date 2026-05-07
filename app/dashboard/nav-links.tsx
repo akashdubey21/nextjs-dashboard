40e5f8b4f4c25878a35ed6906bb2f43ff74ef26d
akashdubey21 <ashdubey0230@gmail.com>
2026-05-07 17:19:22 +0530

    fix nav-links

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,41 +1,41 @@
 'use client';
- 
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
- 
-// ...
- 
+
+const links = [
+  {
+    name: 'Home',
+    href: '/dashboard',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  {
+    name: 'Customers',
+    href: '/dashboard/customers',
+    icon: UserGroupIcon,
+  },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
- 
-  const links = [
-    {
-      name: 'Home',
-      href: '/dashboard',
-      icon: HomeIcon,
-    },
-    {
-      name: 'Invoices',
-      href: '/dashboard/invoices',
-      icon: DocumentDuplicateIcon,
-    },
-    {
-      name: 'Customers',
-      href: '/dashboard/customers',
-      icon: UserGroupIcon,
-    },
-  ];
- 
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+
         return (
           <Link
             key={link.name}
